4bccdb59c96a05b94153ddfd1d03a991f3a40304
Arthur Andrade <arthurandradenf@gmail.com>
2021-07-19 20:04:00 -0300

    Mostra dados da busca

--- scripts/searchAPI.js
+++ scripts/searchAPI.js
@@ -2,6 +2,63 @@ const URL_API = "https://api.coincap.io/v2/assets";
 var request = new XMLHttpRequest()
 let responseJSON;
 
+function buildDataAboutCoin(data) {
+    const { rank, symbol, name, supply, maxSupply, marketCapUsd, priceUsd } = data;
+
+    function convertToMillion(value) {
+        const million = 1000000;
+        return parseFloat(value) / million;
+    }
+
+    function addPrefixMillion(value) {
+        return `${value} M`
+    }
+
+    return {
+        rank,
+        symbol,
+        name,
+        supply: addPrefixMillion(convertToMillion(supply)),
+        maxSupply: addPrefixMillion(convertToMillion(maxSupply)),
+        marketCapUsd: addPrefixMillion(convertToMillion(marketCapUsd)),
+        priceUsd: addPrefixMillion(convertToMillion(priceUsd))
+    }
+}
+
+function buildInfo(name, value) {
+    const info = `  
+        <div class="info">
+            <span class="info-name">${name}</span>
+            <span class="info-value">${value}</span>
+        </div>
+    `;
+
+    return info;
+}
+
+function showModalData(data) {
+
+    const modal = document.getElementById("data-modal");
+    if (modal) {
+        modal.classList.add('show');
+        const container = modal.getElementsByClassName("container")[0];
+        const content = container.getElementsByClassName("content")[0];
+
+        let info = "";
+        for (const key in data) {
+            info += buildInfo(key, data[key])
+        }
+        content.innerHTML = info;
+
+        modal.addEventListener('click', (event) => {
+            event.preventDefault()
+            if (event.target.id == 'idModal' || event.target.className == 'closeBtn') {
+                modal.classList.remove('show')
+            }
+        })
+    }
+}
+
 function showOptions(data) {
     const list = document.getElementById("coins")
     let options = "";
@@ -17,17 +74,19 @@ function searchCoin(coinName) {
     const coins = responseJSON.data;
     let coinId;
     for (let index = 0; index < responseJSON.data.length; index++) {
-        if(coins[index].name === coinName ){
+        if (coins[index].name === coinName) {
             coinId = coins[index].id;
             break;
         }
     }
 
-    if(coinId){
+    if (coinId) {
         request.open('GET', URL_API + `/${coinId}`, true)
         request.onreadystatechange = function () {
             if (request.readyState === 4 && request.status === 200) {
-                console.log(JSON.parse(request.responseText))
+                showModalData(
+                    buildDataAboutCoin(JSON.parse(request.responseText).data)
+                );
             }
         }
         request.send()
