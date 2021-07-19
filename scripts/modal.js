79b01d7f853186f76ff5ba1993a120fa8303eccf
Fábio L <f.leonam@hotmail.com>
2021-07-19 11:54:37 -0300

    Adicionado botao de login, modal para fazer login e campo de busca

---
+++ scripts/modal.js
@@ -0,0 +1,17 @@
+function showModal(idModal){
+  const modal = document.getElementById(idModal);
+  if(modal){
+    modal.classList.add('show');//adiciona a estilização (show) do css, ou seja, faz aparecer o modal.
+    modal.addEventListener('click', (event)=>{
+      event.preventDefault()
+      if(event.target.id == 'idModal' || event.target.className == 'closeBtn'){
+        modal.classList.remove('show')
+      }
+    })
+  }
+}
+
+const loginButton = document.querySelector('.loginBtn');
+loginButton.addEventListener('click', () => {
+  showModal('background-modal')
+})
