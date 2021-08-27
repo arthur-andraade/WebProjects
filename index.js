98951a36abbe398ceb1d15c1f18c7d1660d244b7
Arthur Andrade <arthurandradenf@gmail.com>
2021-08-27 20:18:46 -0300

    Setup deploy no Heroku

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const router = require("./routes")
 const connectDB = require("./config/DB")
+const cors = require("cors");
 
 const app = express();
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 connectDB();
