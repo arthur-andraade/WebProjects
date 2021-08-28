08bd21059951a901ae918553a83e8d93713e56c4
Arthur Andrade <arthurandradenf@gmail.com>
2021-08-28 17:18:02 -0300

    Configura porta do Heroku

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.json())
 connectDB();
 app.use(router)
 
-app.listen(process.env.SERVER_PORT || 8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log("Server running... 🚀 ")
 })
