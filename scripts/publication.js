a6b30aef6ee614ce8a2e282e1f9dafd2414248f4
Arthur Andrade <arthurandradenf@gmail.com>
2021-08-29 15:41:45 -0300

    Buscar publicacoes

--- scripts/publication.js
+++ scripts/publication.js
@@ -1,13 +1,15 @@
 const URL_TO_DO_PUBLICATION = "https://projetoweb-server.herokuapp.com/makepub";
+const URL_TO_DO_SEARCH_PUBLICATION = "https://projetoweb-server.herokuapp.com/publication?";
 const idModalPublication = "modal-publication";
 const formPublication = document.getElementById(idModalPublication).getElementsByTagName("form")[0];
 const buttonSubmit = formPublication.getElementsByTagName("button")[0];
+const buttonSearch = document.getElementsByClassName("search")[0].getElementsByClassName("searchBtn")[0]
+const token = localStorage.getItem("token");
 
 function doPublication() {
     const title = document.getElementById("title-pub").value;
     const content = document.getElementById("content-pub").value;
-    const token = localStorage.getItem("token");
-
+    
     fetch(URL_TO_DO_PUBLICATION, {
         method: "POST",
         headers: {
@@ -18,16 +20,35 @@ function doPublication() {
         credentials: "same-origin",
         body: JSON.stringify({ title, content })
     }).then((response) => {
-        console.log(response)
-    }).catch((error) => {
-        console.log(error);
+        return response.json();
+    }).then(json => {
+        console.log(json);
     });
 
     closeModalToDoPublication();
+}
+
+function searchPublication() {
+    const publicationToSearch = document.getElementById("inputSearch").value;
+    const search = `search=${publicationToSearch}`;
+    fetch(URL_TO_DO_SEARCH_PUBLICATION + search, {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        credentials: "same-origin",
+    }).then((response) => {
+        return response.json();
+    }).then((json)=>{
+        console.log(json);
+    })
 }
 
 function closeModalToDoPublication() {
     document.getElementsByClassName('modal-container')[1].classList.remove("show");
 }
 
-buttonSubmit.addEventListener("click", doPublication)
+buttonSubmit.addEventListener("click", doPublication);
+buttonSearch.addEventListener("click" , searchPublication);
