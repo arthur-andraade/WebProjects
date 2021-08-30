68ec8ad619e45b3dc17b8fc66c7a3a498f22bf77
Arthur Andrade <arthurandradenf@gmail.com>
2021-08-29 21:19:08 -0300

    Remove package nao usado

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const cookieParser = require("cookie-parser");
 const router = require("./routes")
 const connectDB = require("./config/DB")
 const cors = require("cors");
 
 const app = express();
 app.use(cors())
-app.use(cookieParser())
 app.use(express.json())
 connectDB();
 app.use(router)
